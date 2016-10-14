4771a8ac519a1bbe9f067a3af04e80ad0f247129
felixarntz <felix-arntz@t-online.de>
2016-10-14 20:02:24 +0200

    implemented manager classes for Core models

---
+++ includes/mvc/managers/comment-manager.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Manager class for comments
+ *
+ * @package LeavesAndLovePluginLib
+ * @since 1.0.0
+ */
+
+namespace Leaves_And_Love\Plugin_Lib\MVC\Managers;
+
+use Leaves_And_Love\Plugin_Lib\Traits\Sitewide_Manager;
+use Leaves_And_Love\Plugin_Lib\Traits\Meta_Manager;
+
+if ( ! class_exists( 'Leaves_And_Love\Plugin_Lib\MVC\Managers\Comment_Manager' ) ) :
+
+/**
+ * Class for a comments manager
+ *
+ * This class represents a comments manager.
+ *
+ * @since 1.0.0
+ */
+class Comment_Manager extends Core_Manager {
+	use Sitewide_Manager, Meta_Manager;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param Leaves_And_Love\Plugin_Lib\DB    $db       The database instance.
+	 * @param Leaves_And_Love\Plugin_Lib\Cache $cache    The cache instance.
+	 * @param array                            $messages Messages printed to the user.
+	 * @param Leaves_And_Love\Plugin_Lib\Meta  $meta     The meta instance. Optional, but required for managers
+	 *                                                   with meta.
+	 */
+	public function __construct( $db, $cache, $messages, $meta = null ) {
+		$this->class_name            = 'Leaves_And_Love\Plugin_Lib\MVC\Models\Comment';
+		$this->collection_class_name = 'Leaves_And_Love\Plugin_Lib\MVC\Collections\Comments_Collection';
+		$this->query_class_name      = 'Leaves_And_Love\Plugin_Lib\MVC\Queries\Comment_Query';
+
+		$this->table_name     = 'comments';
+		$this->cache_group    = 'comment';
+		$this->meta_type      = 'comment';
+		$this->fetch_callback = 'get_comment';
+
+		parent::__construct( $db, $cache, $messages, $meta );
+	}
+
+	/**
+	 * Internal method to insert a new comment into the database.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param array $args Array of column => value pairs for the new database row.
+	 * @return int|false The ID of the new comment, or false on failure.
+	 */
+	protected function insert_into_db( $args ) {
+		return wp_insert_comment( $args );
+	}
+
+	/**
+	 * Internal method to update an existing comment in the database.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param int   $comment_id ID of the comment to update.
+	 * @param array $args       Array of column => value pairs to update in the database row.
+	 * @return bool True on success, or false on failure.
+	 */
+	protected function update_in_db( $comment_id, $args ) {
+		$args['comment_ID'] = $comment_id;
+
+		return (bool) wp_update_comment( $args );
+	}
+
+	/**
+	 * Internal method to delete a comment from the database.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param int $comment_id ID of the comment to delete.
+	 * @return bool True on success, or false on failure.
+	 */
+	protected function delete_from_db( $comment_id ) {
+		return wp_delete_comment( $comment_id, true );
+	}
+}
+
+endif;
---
+++ includes/mvc/managers/core-manager.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Manager class for Core objects
+ *
+ * @package LeavesAndLovePluginLib
+ * @since 1.0.0
+ */
+
+namespace Leaves_And_Love\Plugin_Lib\MVC\Managers;
+
+use Leaves_And_Love\Plugin_Lib\MVC\Manager;
+
+if ( ! class_exists( 'Leaves_And_Love\Plugin_Lib\MVC\Managers\Core_Manager' ) ) :
+
+/**
+ * Base class for a core manager
+ *
+ * This class represents a general core manager.
+ *
+ * @since 1.0.0
+ */
+abstract class Core_Manager extends Manager {
+	/**
+	 * The callback to fetch an item from the database.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 * @var callable
+	 */
+	protected $fetch_callback;
+
+	/**
+	 * Adds a new model to the database.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param array $args Array of column => value pairs for the new database row.
+	 * @return int|false The ID of the new model, or false on failure.
+	 */
+	public function add( $args ) {
+		$id = $this->insert_into_db( $args );
+		if ( ! $id ) {
+			return false;
+		}
+
+		return $id;
+	}
+
+	/**
+	 * Updates an existing model in the database.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param int   $model_id ID of the model to update.
+	 * @param array $args     Array of column => value pairs to update in the database row.
+	 * @return bool True on success, or false on failure.
+	 */
+	public function update( $model_id, $args ) {
+		$model_id = absint( $model_id );
+
+		$result = $this->update_in_db( $model_id, $args );
+		if ( ! $result ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Deletes an model from the database.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param int $model_id ID of the model to delete.
+	 * @return bool True on success, or false on failure.
+	 */
+	public function delete( $model_id ) {
+		$model_id = absint( $model_id );
+
+		$result = $this->delete_from_db( $model_id );
+		if ( ! $result ) {
+			return false;
+		}
+
+		$this->storage_unset( $model_id );
+
+		return true;
+	}
+
+	/**
+	 * Fetches the Core object for a specific ID.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param int $model_id ID of the object to fetch.
+	 * @return object|null The Core object for the requested ID, or null if it does not exist.
+	 */
+	public function fetch( $model_id ) {
+		return call_user_func( $this->fetch_callback, $model_id );
+	}
+
+	/**
+	 * Internal method to insert a new model into the database.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param array $args Array of column => value pairs for the new database row.
+	 * @return int|false The ID of the new model, or false on failure.
+	 */
+	protected abstract function insert_into_db( $args );
+
+	/**
+	 * Internal method to update an existing model in the database.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param int   $model_id ID of the model to update.
+	 * @param array $args     Array of column => value pairs to update in the database row.
+	 * @return bool True on success, or false on failure.
+	 */
+	protected abstract function update_in_db( $model_id, $args );
+
+	/**
+	 * Internal method to delete a model from the database.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param int $model_id ID of the model to delete.
+	 * @return bool True on success, or false on failure.
+	 */
+	protected abstract function delete_from_db( $model_id );
+}
+
+endif;
