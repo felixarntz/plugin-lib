02a04f601ae3e20a8021e86fc17a1b2c7c087d9c
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-01-18 21:33:39 +0100

    Improve code quality of Service_Instantiator class.

--- src/service-instantiator.php
+++ src/service-instantiator.php
@@ -18,6 +18,15 @@ if ( ! class_exists( 'Leaves_And_Love\Plugin_Lib\Service_Instantiator' ) ) :
  * @since 1.0.0
  */
 final class Service_Instantiator {
+	/**
+	 * Contains the service instances for all plugins.
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 * @static
+	 *
+	 * @var array
+	 */
 	private static $instances = array();
 
 	/**
@@ -79,69 +88,127 @@ final class Service_Instantiator {
 			case 'prefix':
 				return $prefix;
 			case 'services':
-				$service_definitions = call_user_func( array( $class_name, 'get_service_definitions' ) );
-
-				$services_arg = array();
-				foreach ( $args as $arg ) {
-					if ( ! is_array( $arg ) ) {
-						continue;
-					}
-
-					foreach ( $arg as $key => $value ) {
-						if ( isset( $service_definitions[ $key ] ) && is_a( $value, $service_definitions[ $key ] ) ) {
-							$services_arg = $arg;
-							break 2;
-						}
-					}
-				}
-
-				$services = array();
-				foreach ( $service_definitions as $name => $class_name ) {
-					if ( isset( $services_arg[ $name ] ) ) {
-						$services[ $name ] = $services_arg[ $name ];
-					} else {
-						$services[ $name ] = self::instantiate( $class_name, $prefix );
-					}
-				}
-
-				return $services;
+				return self::fill_constructor_services( $class_name, $prefix, $args );
 			case 'args':
-				$args_parsers = call_user_func( array( $class_name, 'get_args_parsers' ) );
-
-				foreach ( $args as $arg ) {
-					if ( ! is_array( $arg ) ) {
-						continue;
-					}
-
-					foreach ( $arg as $key => $value ) {
-						if ( isset( $args_parsers[ $key ] ) && ! is_a( $value, 'Leaves_And_Love\Plugin_Lib\Service' ) ) {
-							return $arg;
-						}
-					}
-				}
-
-				return array();
+				return self::fill_constructor_args( $class_name, $args );
 			case 'translations':
-				foreach ( $args as $arg ) {
-					if ( is_object( $arg ) && is_a( $arg, 'Leaves_And_Love\Plugin_Lib\Translations\Translations' ) ) {
-						return $arg;
-					}
-				}
-
-				$class_name_parts = explode( '\\', $class_name );
-				if ( count( $class_name_parts ) < 3 ) {
-					return null;
-				}
-
-				$translations_class_name = $class_name_parts[0] . '\\' . $class_name_parts[1] . '\\Translations\\Translations_' . array_pop( $class_name_parts );
-				if ( ! class_exists( $translations_class_name ) ) {
-					return null;
-				}
-
-				return new $translations_class_name();
+				return self::fill_constructor_translations( $class_name, $args );
 		}
+
+		return null;
 	}
 
+	/**
+	 * Fills the $services parameter of a constructor.
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 * @static
+	 *
+	 * @param string $class_name Fully qualified service class name.
+	 * @param string $prefix     Prefix for the plugin the class should be instantiated for.
+	 * @param array  $args       Further arguments.
+	 * @return array Array of required service instances.
+	 */
+	private static function fill_constructor_services( $class_name, $prefix, $args ) {
+		$service_definitions = call_user_func( array( $class_name, 'get_service_definitions' ) );
+
+		$services_arg = array();
+		foreach ( $args as $arg ) {
+			if ( ! is_array( $arg ) ) {
+				continue;
+			}
+
+			foreach ( $arg as $key => $value ) {
+				if ( isset( $service_definitions[ $key ] ) && is_a( $value, $service_definitions[ $key ] ) ) {
+					$services_arg = $arg;
+					break 2;
+				}
+			}
+		}
+
+		$services = array();
+		foreach ( $service_definitions as $name => $class_name ) {
+			if ( isset( $services_arg[ $name ] ) ) {
+				$services[ $name ] = $services_arg[ $name ];
+			} else {
+				$services[ $name ] = self::instantiate( $class_name, $prefix );
+			}
+		}
+
+		return $services;
+	}
+
+	/**
+	 * Fills the $args parameter of a constructor.
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 * @static
+	 *
+	 * @param string $class_name Fully qualified service class name.
+	 * @param array  $args       Further arguments.
+	 * @return array Array of arguments.
+	 */
+	private static function fill_constructor_args( $class_name, $args ) {
+		$args_parsers = call_user_func( array( $class_name, 'get_args_parsers' ) );
+
+		foreach ( $args as $arg ) {
+			if ( ! is_array( $arg ) ) {
+				continue;
+			}
+
+			foreach ( $arg as $key => $value ) {
+				if ( isset( $args_parsers[ $key ] ) && ! is_a( $value, 'Leaves_And_Love\Plugin_Lib\Service' ) ) {
+					return $arg;
+				}
+			}
+		}
+
+		return array();
+	}
+
+	/**
+	 * Fills the $translations parameter of a constructor.
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 * @static
+	 *
+	 * @param string $class_name Fully qualified service class name.
+	 * @param array  $args       Further arguments.
+	 * @return Leaves_And_Love\Plugin_Lib\Translations\Translations|null Translations object, or null on failure.
+	 */
+	private static function fill_constructor_translations( $class_name, $args ) {
+		foreach ( $args as $arg ) {
+			if ( is_object( $arg ) && is_a( $arg, 'Leaves_And_Love\Plugin_Lib\Translations\Translations' ) ) {
+				return $arg;
+			}
+		}
+
+		$class_name_parts = explode( '\\', $class_name );
+		if ( count( $class_name_parts ) < 3 ) {
+			return null;
+		}
+
+		$translations_class_name = $class_name_parts[0] . '\\' . $class_name_parts[1] . '\\Translations\\Translations_' . array_pop( $class_name_parts );
+		if ( ! class_exists( $translations_class_name ) ) {
+			return null;
+		}
+
+		return new $translations_class_name();
+	}
+
+	/**
+	 * Returns the parameters for a specific class constructor.
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 * @static
+	 *
+	 * @param ReflectionClass $reflected_class The reflected class.
+	 * @return array Array of ReflectionParam elements.
+	 */
 	private static function get_constructor_params( $reflected_class ) {
 		$reflected_constructor = $reflected_class->getConstructor();
 		if ( ! $reflected_constructor ) {
