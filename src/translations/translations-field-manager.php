76fa5225fd406a036b28aeb56b35d0937c4c8f3b
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-02-27 16:55:15 +0100

    Add translations base for field manager.

--- src/translations/translations-field-manager.php
+++ src/translations/translations-field-manager.php
@@ -25,7 +25,15 @@ class Translations_Field_Manager extends Translations {
 	protected function init() {
 		$this->translations = array(
 			/* translators: %s: field label */
-			'field_empty_required' => $this->__translate( 'No value was given for the required field &#8220;%s&#8221;.', 'textdomain' ),
+			'field_empty_required'           => $this->__translate( 'No value was given for the required field &#8220;%s&#8221;.', 'textdomain' ),
+			/* translators: %s: field label */
+			'field_repeatable_not_array'     => $this->__translate( 'The value for the repeatable field &#8220;%s&#8221; is not an array.', 'textdomain' ),
+			/* translators: %s: field label */
+			'field_repeatable_has_errors'    => $this->__translate( 'One or more errors occurred for the repeatable field &#8220;%s&#8221;.', 'textdomain' ),
+			/* translators: %s: field label */
+			'field_repeatable_add_button'    => $this->__translate( 'Add<span class="screen-reader-text"> another item to the &#8220;%s&#8221; list</span>', 'textdomain' ),
+			/* translators: %s: field label */
+			'field_repeatable_remove_button' => $this->__translate( 'Remove<span class="screen-reader-text"> this item from the &#8220;%s&#8221; list</span>', 'textdomain' ),
 		);
 	}
 }
