c5331241b2382d3e07969a5048cad979de93cf8c
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-01 10:47:42 +0100

    Support optgroups for select fields.

--- src/fields/select.php
+++ src/fields/select.php
@@ -28,6 +28,62 @@ class Select extends Select_Base {
 	protected $slug = 'select';
 
 	/**
+	 * Option groups with choices to select from, if necessary.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 * @var array
+	 */
+	protected $optgroups = array();
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param Leaves_And_Love\Plugin_Lib\Fields\Field_Manager $manager Field manager instance.
+	 * @param string                                          $id      Field identifier.
+	 * @param array                                           $args    {
+	 *     Optional. Field arguments. Anything you pass in addition to the default supported arguments
+	 *     will be used as an attribute on the input. Default empty array.
+	 *
+	 *     @type string          $section       Section identifier this field belongs to. Default empty.
+	 *     @type string          $label         Field label. Default empty.
+	 *     @type string          $description   Field description. Default empty.
+	 *     @type mixed           $default       Default value for the field. Default null.
+	 *     @type bool|int        $repeatable    Whether this should be a repeatable field. An integer can also
+	 *                                          be passed to set the limit of repetitions allowed. Default false.
+	 *     @type array           $input_classes Array of CSS classes for the field input. Default empty array.
+	 *     @type array           $label_classes Array of CSS classes for the field label. Default empty array.
+	 *     @type callable        $validate      Custom validation callback. Will be executed after doing the regular
+	 *                                          validation if no errors occurred in the meantime. Default none.
+	 *     @type callable|string $before        Callback or string that should be used to generate output that will
+	 *                                          be printed before the field. Default none.
+	 *     @type callable|string $after         Callback or string that should be used to generate output that will
+	 *                                          be printed after the field. Default none.
+	 * }
+	 */
+	public function __construct( $manager, $id, $args = array() ) {
+		parent::__construct( $manager, $id, $args );
+
+		if ( ! empty( $this->optgroups ) ) {
+			if ( ! empty( $this->choices ) ) {
+				array_unshift( $this->optgroups, array(
+					'label'   => '',
+					'choices' => $this->choices,
+				) );
+			}
+
+			$this->choices = array();
+
+			foreach ( $this->optgroups as $optgroup ) {
+				$this->choices = array_merge( $this->choices, $optgroup['choices'] );
+			}
+		}
+	}
+
+	/**
 	 * Renders a single input for the field.
 	 *
 	 * @since 1.0.0
@@ -44,14 +100,35 @@ class Select extends Select_Base {
 
 		?>
 		<select<?php echo $this->get_input_attrs( $input_attrs ); ?>>
-			<?php foreach ( $this->choices as $value => $label ) :
-				$option_attrs = array(
-					'value'    => $value,
-					'selected' => in_array( $value, $current_value, true ),
-				);
-				?>
-				<option<?php echo $this->attrs( $option_attrs ); ?>><?php echo esc_html( $label ); ?></option>
-			<?php endforeach; ?>
+			<?php if ( ! empty( $this->optgroups ) ) : ?>
+				<?php foreach ( $this->optgroups as $optgroup ) : ?>
+					<?php if ( ! empty( $optgroup['label'] ) ) : ?>
+						<optgroup label="<?php echo esc_attr( $optgroup['label'] ); ?>">
+					<?php endif; ?>
+
+					<?php foreach ( $optgroup['choices'] as $value => $label ) :
+						$option_attrs = array(
+							'value'    => $value,
+							'selected' => in_array( $value, $current_value, true ),
+						);
+						?>
+						<option<?php echo $this->attrs( $option_attrs ); ?>><?php echo esc_html( $label ); ?></option>
+					<?php endforeach; ?>
+
+					<?php if ( ! empty( $optgroup['label'] ) ) : ?>
+						</optgroup>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			<?php else : ?>
+				<?php foreach ( $this->choices as $value => $label ) :
+					$option_attrs = array(
+						'value'    => $value,
+						'selected' => in_array( $value, $current_value, true ),
+					);
+					?>
+					<option<?php echo $this->attrs( $option_attrs ); ?>><?php echo esc_html( $label ); ?></option>
+				<?php endforeach; ?>
+			<?php endif; ?>
 		</select>
 		<?php
 		$this->render_repeatable_remove_button();
@@ -74,12 +151,44 @@ class Select extends Select_Base {
 
 		?>
 		<select{{ _.attrs( data.input_attrs ) }}<?php echo $multiple; ?>>
-			<# _.each( data.choices, function( label, value ) { #>
-				<option value="{{ value }}"<?php echo $selected; ?>>{{ label }}</option>
-			<# } ) #>
+			<# if ( ! _.isEmpty( data.optgroups ) ) { #>
+				<# _.each( data.optgroups, function( optgroup ) { #>
+					<# if ( ! _.isEmpty( optgroup.label ) ) { #>
+						<optgroup label="{{ optgroup.label }}">
+					<# } #>
+
+					<# _.each( optgroup.choices, function( label, value ) { #>
+						<option value="{{ value }}"<?php echo $selected; ?>>{{ label }}</option>
+					<# } ) #>
+
+					<# if ( ! _.isEmpty( optgroup.label ) ) { #>
+						</optgroup>
+					<# } #>
+				<# }) #>
+			<# } else { #>
+				<# _.each( data.choices, function( label, value ) { #>
+					<option value="{{ value }}"<?php echo $selected; ?>>{{ label }}</option>
+				<# } ) #>
+			<# } #>
 		</select>
 		<?php
 		$this->print_repeatable_remove_button_template();
+	}
+
+	/**
+	 * Transforms single field data into an array to be passed to JavaScript applications.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param mixed $current_value Current value of the field.
+	 * @return array Field data to be JSON-encoded.
+	 */
+	protected function single_to_json( $current_value ) {
+		$data = parent::single_to_json( $current_value );
+		$data['optgroups'] = $this->optgroups;
+
+		return $data;
 	}
 }
 
