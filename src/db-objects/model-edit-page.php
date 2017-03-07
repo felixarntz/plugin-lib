9def2d5c8453158752a33b7b82abb01165ee6484
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-07 16:03:58 +0100

    Add view link to model edit page if applicable.

--- src/db-objects/model-edit-page.php
+++ src/db-objects/model-edit-page.php
@@ -344,7 +344,13 @@ abstract class Model_Edit_Page extends Manager_Page {
 
 		$preview_text = $id ? $manager->get_message( 'edit_page_preview_changes' ) : $manager->get_message( 'edit_page_preview' );
 
-		?>
+		$permalink = $view_routing->get_model_permalink( $model );
+
+		if ( ! empty( $permalink ) ) : ?>
+			<div id="view-action">
+				<a class="button" href="<?php echo esc_url( $permalink ); ?>" target="_blank"><?php echo $manager->get_message( 'edit_page_view' ); ?></a>
+			</div>
+		<?php endif; ?>
 		<div id="preview-action">
 			<button id="post-preview" class="preview button"><?php echo $preview_text; ?></button>
 		</div>
